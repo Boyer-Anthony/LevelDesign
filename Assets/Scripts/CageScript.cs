781ae955b029ea0fe90fbebb867542883594ddf1
Boyer-Anthony <a.boyer@rubika-edu.re>
2025-02-20 21:40:02 +0400

    push Level Design Jouable

---
+++ Assets/Scripts/CageScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageScript : MonoBehaviour
+{
+   
+    public GameObject Cage; // L'objet à détruire
+    public int collectedObjects = 0;
+    private int requiredObjects = 5; // Nombre d'objets requis
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Vérifier si l'objet collecté est un des objets spécifiques
+        if (other.CompareTag("Collectible"))
+        {
+            collectedObjects++;
+            Destroy(other.gameObject); // Détruit l'objet collecté
+
+            // Vérifier si tous les objets requis ont été collectés
+            if (collectedObjects >= requiredObjects && Cage != null)
+            {
+                Destroy(Cage); // Détruit l'objet cible
+                Debug.Log("Tous les objets ont été collectés. Objet cible détruit !");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 2f;
+    private float rotationX = 0f;
+
+    void Update()
+    {
+        // Déplacement du joueur
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(new Vector3(moveX, 0, moveZ));
+
+        // Rotation de la caméra
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
+
